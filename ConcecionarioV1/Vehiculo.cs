d739344cbb1ad1874d125af24f73cce23b998582
alexcaneteantolinez <156774941+alexcaneteantolinez@users.noreply.github.com>
2024-01-29 12:23:00 +0100

    Estructura Clase Vehiculo
    Con comentarios creada la estructura principal

--- ConcecionarioV1/Vehiculo.cs
+++ ConcecionarioV1/Vehiculo.cs
@@ -8,6 +8,16 @@ namespace ConcecionarioV1
 {
     public class Vehiculo
     {
+        // Constantes
 
+        // Miembros Privados
+
+        // Constructores
+        
+        // Propiedades
+
+        // Metodos Publicos
+
+        // Metodos Privados
     }
 }
