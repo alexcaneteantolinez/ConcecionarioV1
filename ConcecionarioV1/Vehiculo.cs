b607581c72bddf9dfec96893e946146bdadd7c11
alexcaneteantolinez <alxcaant@gmail.com>
2024-01-29 12:37:15 +0100

    Clase Vehiculo: -Miembros Privados -Propiedades -Constructores

--- ConcecionarioV1/Vehiculo.cs
+++ ConcecionarioV1/Vehiculo.cs
@@ -11,11 +11,40 @@ namespace ConcecionarioV1
         // Constantes
 
         // Miembros Privados
-
+        private string _marca;
+        private string _modelo;
         // Constructores
-        
+        public Vehiculo(string marca, string modelo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+        }
         // Propiedades
+        public string Marca
+        {
+            get
+            {
+                return _marca;
+            }
+            set
+            {
+                // TODO: Proporcionar seguridad y proteccion al miembro -> _marca
 
+                _marca = value;
+            }
+        }
+        public string Modelo
+        {
+            get
+            {
+                return _modelo;
+            }
+            set 
+            {
+                // TODO: Proporcionar seguridad y proteccion al miembro -> _modelo
+                _modelo = value; 
+            }
+        }
         // Metodos Publicos
 
         // Metodos Privados
