15680d8bc2567ad27165c33df8dde435d8e61e64
alexcaneteantolinez <alxcaant@gmail.com>
2024-01-29 12:04:35 +0100

    Agregar archivos de proyecto.

---
+++ ConcecionarioV1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConcecionarioV1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Controlador principal de la aplicacion
+
+            // Menu De Acciones a Realizar
+
+            // Proceso de las acciones
+        }
+    }
+}
